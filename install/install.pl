e31f34d9a546c6662a388737e8ec2d5484e31367
Casper Joost Eyckelhof <joost@multigator.net>
2005-10-15 18:04:36 +0000

    initial commit of install command
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@4 07fb6d0f-9701-0410-8146-bd1aaf089aca

---
+++ install/install.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -w
+# Tries to install a command
+
+use strict;
+
+my $multidir = $ENV{MULTI_ROOT};
+my $dstdir = "${multidir}/commands";
+#Fixme, get from configfile
+my $repos = 'https://svn.muze.nl/svn/multigate_commands/trunk/';
+my $svn_user = '';
+my $svn = '/usr/bin/svn';
+
+unless (defined $multidir and -d $dstdir) {
+  print "Installation directory \"$dstdir\" undefined or invalid\n";
+  exit 0;
+}
+
+unless (-x $svn) {
+  print "svn not available on path \"$svn\"";
+  exit 0;
+}
+
+my $command = defined $ARGV[0] ? $ARGV[0] : '';
+
+unless ($command =~ /^\w+$/) {
+   print "Invalid characters in command: \"$command\"\n";
+   exit 0;
+}
+
+if (-d "$dstdir/$command") {
+   print "Command already installed!\n";
+   exit 0;
+}
+
+my $svn_url = $repos . $command;
+#my @cmd = ('svn', 'co' , '--username', $svn_user , $svn_url, "$dstdir/$command");
+#my $result = (system(@cmd) == 0);
+
+#Do the svn stuff!
+my $credentials = ( (defined $svn_user and $svn_user =~ /^\w+$/ ) ? "--username $svn_user" : "");
+my $commandline = "$svn checkout $credentials $svn_url $dstdir/$command";
+print STDERR "commandline = $commandline\n";
+my $pid = open( README, "$commandline |") or die "Couldn't fork svn: $!\n";
+my $files = 0 ;
+while (my $line = <README>) {
+   #we expect a few "A filename", followed by "Checked out revision n."
+   chomp $line;
+   if ($line =~ /^A\s+.*?$/) {
+     #looks OK...
+     $files++;
+   } elsif ($line =~ /^Checked out revision (\d+)\.$/) {
+     #Tada!
+     print "Done ($files files, rev $1)\n";
+   } else {
+     #problem?
+     print STDERR "svn output: $line\n";
+     print "Problem with checkout, see console for more info\n";
+   }
+}
+close README;
