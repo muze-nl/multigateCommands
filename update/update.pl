d9dd798f573b75ca413243c6e7d63d3b81faa8f5
Casper Joost Eyckelhof <joost@multigator.net>
2005-10-15 20:06:20 +0000

    initial commit for update... might not even work, but needed for 'bootstrapping' the update system
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@5 07fb6d0f-9701-0410-8146-bd1aaf089aca

---
+++ update/update.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+# Tries to update a command
+
+use strict;
+
+my $multidir = $ENV{MULTI_ROOT};
+my $dstdir = "${multidir}/commands";
+#Fixme, get from configfile
+my $repos = 'https://svn.muze.nl/svn/multigate_commands/trunk/';
+my $svn_user = '';
+my $svn = '/usr/bin/svn';
+
+my $command = defined $ARGV[0] ? $ARGV[0] : '';
+
+unless (-d "$dstdir/$command") {
+  print "Command \"$command\" not installed, unable to update\n";
+  exit 0;
+}
+
+unless (-x $svn) {
+  print "svn not available on path \"$svn\"";
+  exit 0;
+}
+
+unless ($command =~ /^\w+$/) {
+   print "Invalid characters in command: \"$command\"\n";
+   exit 0;
+}
+
+my $svn_url = $repos . $command;
+
+#Do the svn stuff!
+my $credentials = ( (defined $svn_user and $svn_user =~ /^\w+$/ ) ? "--username $svn_user" : "");
+my $commandline = "$svn update $credentials $svn_url";
+print STDERR "commandline = $commandline\n";
+my $pid = open( README, "$commandline |") or die "Couldn't fork svn: $!\n";
+my $files = 0 ;
+while (my $line = <README>) {
+   chomp $line;
+   if ($line =~ /^[AUD]\s+.*?$/) {
+     #looks OK...
+     $files++;
+   } elsif ($line =~ /^Checked out revision (\d+)\.$/) {
+     #Tada!
+     print "Done ($files files updated, rev $1)\n";
+   } else {
+     #problem?
+     print STDERR "svn output: $line\n";
+     print "Possible problem with update, see console for more info\n";
+   }
+}
+close README;
