6cc3ffd1d8f53f7ac0ab852a587c724c5d22b60c
Robbert Müller <muller@muze.nl>
2005-10-30 11:26:00 +0000

    gogogo
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@16 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- zomertijd/zomertijd.pl
+++ zomertijd/zomertijd.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/perl -w
 
-my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst ) = localtime( time - 3600 );
+my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst ) = localtime( time );
 
-printf( "Eigenlijk is het pas %02d:%02d.\n", $hour, $min );
+if($isdst == 0){
+	( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst ) = localtime( time + 3600 );
+	printf( "Het had ook %02d:%02d kunnen zijn.\n", $hour, $min );
+} else {
+	( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst ) = localtime( time - 3600 );
+	printf( "Eigenlijk is het pas %02d:%02d.\n", $hour, $min );
+}
