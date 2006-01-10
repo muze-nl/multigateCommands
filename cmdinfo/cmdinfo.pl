d26ade8f89a7198ca2facfb725670f1aec99213e
Robbert Müller <muller@muze.nl>
2006-01-10 10:43:56 +0000

    Woei svn info nu opvraagbaar
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@60 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- cmdinfo/cmdinfo.pl
+++ cmdinfo/cmdinfo.pl
@@ -8,8 +8,11 @@ use lib "$multiroot/lib";
 use Multigate::Config qw (read_commandconfig);
 
 my $commandline = defined $ARGV[0] ? $ARGV[0] : '';
-my ( $commandname, undef ) = split " ", $commandline, 2;
-$commandname = lc($commandline);
+my ( $commandname,  $flag) = split " ", $commandline, 2;
+$commandname = defined $commandname  ?  $commandname : '';
+$flag = defined $flag  ?  $flag : '';
+$commandname = lc($commandname);
+
 
 unless ( -d "$multiroot/commands/$commandname" and ( $commandname =~ /^\w+$/ ) ) {
     print "Sorry, \"$commandname\" is not a valid command name\n";
@@ -26,4 +29,13 @@ else {
         print "author: $config{author}\n";
     }
     # Add more here
+	 if( $flag eq 'svn') {
+		 if( -e "$multiroot/commands/$commandname/.svn" ) {
+			 my $svninfo = `svn info $multiroot/commands/$commandname`; 
+			 print "SVN info:\n";
+			 print $svninfo;
+		 } else {
+			 print "SVN info: not a svn mannaged command\n";
+		 }
+	}
 }
