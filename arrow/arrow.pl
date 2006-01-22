2dec47d9f3b9f0f6764788a698e8b5ba9ae1c387
Casper Joost Eyckelhof <joost@multigator.net>
2006-01-22 13:54:32 +0000

    changed arrow to work again with new arrow-website (some replay attack is needed ;)
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@65 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- arrow/arrow.pl
+++ arrow/arrow.pl
@@ -1,39 +1,74 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl 
 # Casper Joost Eyckelhof (Titanhead)
 # casper@joost.student.utwente.nl
+
+use strict;
+use warnings;
 
 use LWP::UserAgent;
 use HTTP::Cookies;
 
-$ua = new LWP::UserAgent;
+
+my $ua = new LWP::UserAgent;
+my $cookie_jar = HTTP::Cookies->new;
 
 #### allerlei fijne definities en initialisaties ########
 
-@agents = (
+my @agents = (
     "Mozilla/4.0 (compatible; MSIE 4.01; Windows 98)", "Mozilla/4.0 (compatible; MSIE 5.0; Windows 98; DigExt)",
     "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)"
 );
 
-$agent = @agents[ int( rand(@agents) ) ];
+my $agent = @agents[ int( rand(@agents) ) ];
 $ua->agent($agent);
 
-$request = new HTTP::Request( 'GET', "http://www.arrow.nl/jazz/" );
-$request->referer("http://www.arrow.nl/");
-$response = $ua->request($request);
-$html     = $response->content;
-@lines    = split /^/m, $html;
 
-# <div name="playlist" id="playlist" style="margin: 5px;">\s*(.*?)\s*</div>
+#Get my first cookie!
+my $request = new HTTP::Request( 'GET', "http://www.arrow.nl/javascript/default.js/" );
+$request->header( "Accept"          => "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, image/png" );
+$request->header( "Accept-Encoding" => "gzip" );
+$request->header( "Accept-Language" => "en" );
+$request->header( "Accept-Charset"  => "iso-8859-1,*,utf-8" );
+my $response = $ua->request($request);
+$cookie_jar->extract_cookies($response);
 
-foreach my $line (@lines) {
-   print "$line\n";
+my $cookie_string = $cookie_jar->as_string();
+
+my $session;
+if ($cookie_string =~ /PHPSESSID=(\w+);/ ) {
+  $session = $1;
+  #print STDERR "Session: $session\n";
+} else {
+  print "Problem getting session from server\n";
+  exit 0;
 }
 
-exit 0;
- 
-#print STDERR $resultline , "\n";
-#if ( $resultline =~ m/\s*(.*?)-(.*?)\s*$/) {
-#   print lc("Now playing on arrow jazz: $2 (by $1)\n");
-#} else {
-#   print "Unable to find current song for arrow jazz\n";
-#}
+my $replay = 'O%3A8%3A%22stdClass%22%3A1%3A%7Bs%3A5%3A%22_data%22%3Ba%3A6%3A%7Bs%3A1%3A%220%22%3Bi%3A1%3Bs%3A1%3A%221%22%3Bs%3A14%3A%22Handler_Remote%22%3Bs%3A1%3A%222%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Bs%3A11%3A%22getPlaylist%22%3B%7Ds%3A1%3A%223%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Bs%3A4%3A%22jazz%22%3B%7D%7Ds%3A1%3A%224%22%3Bs%3A6%3A%22secret%22%3Bs%3A1%3A%229%22%3Bs%3A10%3A%22javascript%22%3B%7D%7D';
+
+$request = new HTTP::Request( 'POST', "http://www.arrow.nl/services/Gateway.php" );
+$request->referer("http://www.arrow.nl/jazz/");
+$request->header( "Accept"          => "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, image/png" );
+$request->header( "Accept-Encoding" => "gzip" );
+$request->header( "Accept-Language" => "en" );
+$request->header( "Accept-Charset"  => "iso-8859-1,*,utf-8" );
+$request->content( $replay );
+
+$cookie_jar->add_cookie_header($request);
+
+$response = $ua->request($request);
+my $content  = $response->content;
+
+# content is now a serialized PHP-like object
+# Example: 
+# 1O:14:"handler_remote":5:{s:4:"user";N;s:5:"error";N;s:7:"_mailer";N;s:6:"_crypt";N;s:7:"_loader";O:8:"stdClass":2:{s:12:"serverResult";O:8:"stdClass":1:{s:6:"huidig";O:8:"stdClass":1:{s:7:"artiest";s:49:"Erma Franklin - (take A Little) Piece Of My Heart";}}s:3:"SID";s:32:"f25780e412412834a0ae3f30a55f1398";}}
+#
+# Interesting part:
+# "stdClass":1:{s:7:"artiest";s:49:"Erma Franklin - (take A Little) Piece Of My Heart";}
+# Lets just regex it, not parse it :)
+
+if ( $content =~ /"stdClass":1:{s:7:"artiest";s:\d+:"(.*?)";}/) {
+   my $artist = $1;
+   print "Now playing: $artist\n";
+} else {
+   print "Problem interpreting result from arrow...\n";
+}
