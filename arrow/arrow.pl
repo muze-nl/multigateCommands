e3245f2be5ff9e57ae6fbbbe639e419aa7e1d241
Casper Joost Eyckelhof <joost@multigator.net>
2006-01-22 14:12:00 +0000

    added switch for jazz/rock
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@66 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- arrow/arrow.pl
+++ arrow/arrow.pl
@@ -8,6 +8,14 @@ use warnings;
 use LWP::UserAgent;
 use HTTP::Cookies;
 
+my $commandline = $ARGV[0];
+$commandline = (defined $commandline) ? $commandline : '';
+
+my $type = 'jazz';
+if ($commandline =~ /rock/ ) {  #somewhere
+  $type = 'rock';
+}
+
 
 my $ua = new LWP::UserAgent;
 my $cookie_jar = HTTP::Cookies->new;
@@ -43,10 +51,11 @@ if ($cookie_string =~ /PHPSESSID=(\w+);/ ) {
   exit 0;
 }
 
-my $replay = 'O%3A8%3A%22stdClass%22%3A1%3A%7Bs%3A5%3A%22_data%22%3Ba%3A6%3A%7Bs%3A1%3A%220%22%3Bi%3A1%3Bs%3A1%3A%221%22%3Bs%3A14%3A%22Handler_Remote%22%3Bs%3A1%3A%222%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Bs%3A11%3A%22getPlaylist%22%3B%7Ds%3A1%3A%223%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Bs%3A4%3A%22jazz%22%3B%7D%7Ds%3A1%3A%224%22%3Bs%3A6%3A%22secret%22%3Bs%3A1%3A%229%22%3Bs%3A10%3A%22javascript%22%3B%7D%7D';
+# $replay contains a variable $type that should be set to 'rock' or 'jazz' by now :)
+my $replay = "O%3A8%3A%22stdClass%22%3A1%3A%7Bs%3A5%3A%22_data%22%3Ba%3A6%3A%7Bs%3A1%3A%220%22%3Bi%3A1%3Bs%3A1%3A%221%22%3Bs%3A14%3A%22Handler_Remote%22%3Bs%3A1%3A%222%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Bs%3A11%3A%22getPlaylist%22%3B%7Ds%3A1%3A%223%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Ba%3A1%3A%7Bs%3A1%3A%220%22%3Bs%3A4%3A%22$type%22%3B%7D%7Ds%3A1%3A%224%22%3Bs%3A6%3A%22secret%22%3Bs%3A1%3A%229%22%3Bs%3A10%3A%22javascript%22%3B%7D%7D";
 
 $request = new HTTP::Request( 'POST', "http://www.arrow.nl/services/Gateway.php" );
-$request->referer("http://www.arrow.nl/jazz/");
+$request->referer("http://www.arrow.nl/$type/");
 $request->header( "Accept"          => "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, image/png" );
 $request->header( "Accept-Encoding" => "gzip" );
 $request->header( "Accept-Language" => "en" );
@@ -66,9 +75,12 @@ my $content  = $response->content;
 # "stdClass":1:{s:7:"artiest";s:49:"Erma Franklin - (take A Little) Piece Of My Heart";}
 # Lets just regex it, not parse it :)
 
+$content =~ s/\n/ - /g; #rock uses a newline, jazz uses a -
+
 if ( $content =~ /"stdClass":1:{s:7:"artiest";s:\d+:"(.*?)";}/) {
    my $artist = $1;
-   print "Now playing: $artist\n";
+   print "Now playing on arrow $type: $artist\n";
 } else {
    print "Problem interpreting result from arrow...\n";
+   print STDERR $content, "\n";
 }
