9a351884e31bc551fe3aba6ce45a553379389cab
Casper Joost Eyckelhof <joost@multigator.net>
2006-03-16 23:14:14 +0000

    tsd command, implemented by wieger
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@73 07fb6d0f-9701-0410-8146-bd1aaf089aca

---
+++ tsd/tsd.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $number = 0;
+
+if ( ( defined( $ARGV[0] ) ) && ( $ARGV[0] =~ /(\d+)/ ) ) {
+
+    #het is een nummertje
+    $number = $1;
+}
+
+#Inlezen file
+
+open( FACTS, "< tsd.txt" );
+
+my @facts;
+my @numfacts;
+
+while ( my $line = <FACTS> ) {
+    chomp $line;
+    push @facts, $line;
+}
+
+close FACTS;
+
+my $rule = $facts[ int( rand(@facts) ) ];
+
+#if ( $number > 0 ) {
+#    if ( defined $numfacts[$number] ) {
+#
+#        #arg is gegeven en rule nummer $arg bestaat
+#        print "$numfacts[$number]";
+#        exit 0;
+#    }
+#}
+
+#pak random rule en print.
+print $rule;
