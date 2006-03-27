ca3ab7012e649007a61f6cecce3e4ae29a71accf
Casper Joost Eyckelhof <joost@multigator.net>
2006-03-27 09:20:11 +0000

    fixed parsing issue with rock
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@76 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- arrow/arrow.pl
+++ arrow/arrow.pl
@@ -75,6 +75,7 @@ my $content  = $response->content;
 # "stdClass":1:{s:7:"artiest";s:49:"Erma Franklin - (take A Little) Piece Of My Heart";}
 # Lets just regex it, not parse it :)
 
+$content =~ s/Error.*?\n//g; #some issues with 'rock', workaround
 $content =~ s/\n/ - /g; #rock uses a newline, jazz uses a -
 
 if ( $content =~ /"stdClass":1:{s:7:"artiest";s:\d+:"(.*?)";}/) {
