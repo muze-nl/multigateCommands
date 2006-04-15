60f0787221da2592ae78be08be37033fc5be4572
Casper Joost Eyckelhof <joost@multigator.net>
2006-04-15 13:09:25 +0000

    Changed from (broken) euroglot.nl to interglot.com
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@87 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- vertaal/vertaal.pl
+++ vertaal/vertaal.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl 
-# Arjan Opmeer (Ado)
-# ado@dnd.utwente.nl
+# Based on framework by Ado
 
 use strict;
 use LWP::UserAgent;
@@ -14,38 +13,28 @@ my $multicast = $ENV{'MULTI_IS_MULTICAST'};
 my $maxlines  = 4;                            #maxlines on multicast channel
 
 my %langtable = (
-    'e' => "Engels",
-    'n' => "Nederlands",
-    'f' => "Frans",
-    'd' => "Duits",
-    'i' => "Italiaans",
-    's' => "Spaans"
+    'e' => 2,
+    'n' => 1,
+    'f' => 4,
+    'd' => 3,
+#   's' => 6,  #(zweeds)
+    's' => 5,  #(spaans)
 );
 
 my $fromlang = $langtable{ substr( $lang, 0, 1 ) };
 my $tolang   = $langtable{ substr( $lang, 1, 1 ) };
-
-#print "from: $fromlang\nto: $tolang\n";
 
 if ( ( !defined $fromlang ) || ( !defined $tolang ) || ( !defined $word ) ) {
     print "Geef bestaande taalcode en 1 woord. (Bv. \"vertaal ne bier\")\n";
     exit(1);
 }
 
-# Construct URL
-my $url =
-  "http://www.euroglotonline.nl/scripts/Eginternet/Euroglot.exe?&ScreenLanguage=Dutch&srcLang="
-  . $fromlang
-  . "&dstLang=" . $tolang
-  . "&srcInput=" . $word
-  . "&MorphReg=false&";
-
 # Create a new useragent instance
 my $ua = new LWP::UserAgent;
-
-# Set agent name. Don't let the other side know that we are a script
 my $agent = "Mozilla/4.0 (compatible; MSIE 4.01; Windows 98)";
 $ua->agent($agent);
+
+my $url = "http://www.interglot.com/resultpage.php?word=$word&SrcLang=$fromlang&DstLang=$tolang";
 
 # Fetch the requested page
 my $request = new HTTP::Request( 'GET', $url );
@@ -53,47 +42,39 @@ my $result  = $ua->request($request);
 
 if ( !$result->is_success ) {
     print "Error opening page\n";
-    exit(1);
+    exit 1;
 }
 
-my $content = $result->content;
+my @lines = split ( /^/m, $result->content );
 
-# Bleh! Output uses CRLF. Remove CR.
-$content =~ s/\r//sg;
-
-# Strip down output to inner table of nested tables structure
-$content =~ s/^.*?<table.*?>(.*)<\/table>.*?$/$1/si;
-$content =~ s/^.*?<table.*?>(.*)<\/table>.*?$/$1/si;
-$content =~ s/^.*?<table.*?>(.*)<\/table>.*?$/$1/si;
-
-# Remove remaining HTML markup
-$content =~ s/<.*?>//sg;
-
-# Remove leading and trailing whitespace
-$content =~ s/^\s*//mg;
-$content =~ s/\s*$//mg;
-
-my @lines = split ( /^/m, $content );
-chomp(@lines);
-
-# Find matches
-my $result  = "";
-my @results = ();
-my $count   = 0;
-for ( my $i = 0 ; $i < $#lines ; $i = $i + 2 ) {
-    if ( $lines[$i] =~ /^$word/i ) {
-        $count++;
-        $result .= "$count. $lines[$i]: $lines[$i + 1]\n";
-        push @results, "$count. $lines[$i]: $lines[$i + 1]";
-    }
+my @results;
+foreach my $line (@lines) {
+   if ($line =~ m|SrcStar\[(\d+)\]\s*=\s*"(.*?)";.*?| ) {
+      my $pos = $1;
+      my $word = $2;
+      $results[$pos] = [ $word ];   
+   }
+   if ($line =~ m|DestWords\[(\d+)\]\[(\d+)\]\s*=\s*"(.*?)";| ) {
+      my ($pos1, $pos2) = ($1, $2);
+      my $word = $3;
+      push @{$results[$pos1]} ,  $word;
+   }
 }
 
-if ($count) {
-    if ($multicast) {    #print $maxlines lines
-        print join "\n", splice( @results, 0, $maxlines );
-    } else {    #print everything
-        print join "\n", @results;
-    }
+my $cut = ( scalar(@results) > ($maxlines+1) ) and $multicast;  # do not show extra header when you can also show all results!
+if ( $cut  ) {
+   print scalar @results , " results. For complete list try a non-multicats channel.\n";
+    splice @results, $maxlines; 
+}
+
+foreach my $entry (@results) {
+   my $src = shift @$entry;
+   print "$word ($src): " , join(', ', @$entry), "\n";
+}
+
+if ($cut) {
+  exit 1; #to prevent caching
 } else {
-    print "Niets gevonden\n";
+  exit 0;
 }
+
