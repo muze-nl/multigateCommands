bac1d5d47bbb02a185fa4150a52d2c5ee96547e9
Casper Joost Eyckelhof <joost@multigator.net>
2006-07-26 08:45:42 +0000

    fixed grep to actually do something
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@99 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- grep/grep.pl
+++ grep/grep.pl
@@ -1,22 +1,14 @@
 #!/usr/bin/perl -w
+# Simple grep for multi-commands
+# case insensitive; needs more errorchecking
+
 use strict;
-
-## Import available environment variables
-
-my $address       = $ENV{'MULTI_USER'};            # address of invoking user
-my $user          = $ENV{'MULTI_REALUSER'};        # multigate username of invoking user
-my $userlevel     = $ENV{'MULTI_USERLEVEL'};       # userlevel of invoking user
-my $from_protocol = $ENV{'MULTI_FROM'};            # protocol this command was invoked from
-my $to_protocol   = $ENV{'MULTI_TO'};              # protocol where output will be sent
-my $command_level = $ENV{'MULTI_COMMANDLEVEL'};    # level needed for this command
-my $is_multicast  = $ENV{'MULTI_IS_MULTICAST'};    # message to multiple recipients (channels)
-
 
 my $commandline = defined $ARGV[0] ? $ARGV[0] : '';
 
-my ($expression, $payload) = split $commandline, 2;
+my ($expression, $payload) = split " ", $commandline, 2;
 
-my @lines = split /\n/, $payload;
+my @lines = split /\xb6/, $payload;
+my @result = grep { /$expression/i } @lines;
 
-my @result = grep { /$expression/ } @lines;
-
+print join("\n", @result);
