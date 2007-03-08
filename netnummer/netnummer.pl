819da545bba8ed4e6eafa5455553d23929934687
Casper Joost Eyckelhof <joost@multigator.net>
2007-03-08 22:16:04 +0000

    netnummer command by matthijs bomhoff
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@132 07fb6d0f-9701-0410-8146-bd1aaf089aca

---
+++ netnummer/netnummer.pl
@@ -0,0 +1,27 @@
+#! /usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use LWP::UserAgent;
+
+my $nummer = $ARGV[0];
+
+my $url = "http://www.detelefoongids.nl/netnummers.html";
+
+my $result = "fout: nummer niet gevonden, of !netnummer stuk...";
+if (defined $nummer and $nummer =~ m/0\d+/) {
+	my $ua = new LWP::UserAgent;
+	my $agent = "Mozilla/4.0 (compatible; MSIE 4.01; Windows 98)";
+	$ua->agent($agent);
+	my $request = new HTTP::Request( 'GET', $url );
+	my $content = $ua->request($request)->content;
+	if (defined $content) {
+		($result) = $content =~ m!<td>$nummer</td>\s*<td>\s*([^<]*?)\s*</td>!im;
+		$result = "niet gevonden" unless defined $result;
+	}
+} else {
+	$result = "fout: geen (zinnig) netnummer meegegeven";
+}
+
+print $result, "\n";
