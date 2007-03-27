ab9ab9bce4cc13254b9b1b6fa76c01a2acfe5dee
Casper Joost Eyckelhof <joost@multigator.net>
2007-03-27 23:12:19 +0000

    first version for !lynn thanks to Lynn Packwood and some big consulting company for the inspiration
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@133 07fb6d0f-9701-0410-8146-bd1aaf089aca

---
+++ lynn/lynn.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+
+#evil backticks, but no user-input
+my $pass =`./pwgen -c -n 6`;
+chomp $pass;
+
+my %table = ( 
+    A => ["autobus", "aapje", "AOL", "afgeschreven", "amstel" ], 
+    B => ["brandweerauto", ], 
+    C => ["chaos", ], 
+    D => ["dombo", "debiel"], 
+    E => ["eikel", ], 
+    F => ["flupke", ], 
+    G => ["groentje", ], 
+    H => ["halve-zool", "heineken" ], 
+    I => ["incompetent", ], 
+    J => ["josti", ], 
+    K => ["klootviool", "kastanje"], 
+    L => ["looprek", ], 
+    M => ["muts", "mongool"], 
+    N => ["nono", ],
+    O => ["opa", "oma" ],
+    P => ["prutser", "paashaas"],
+    Q => ["quasi-onverschillig", ],
+    R => ["randdebiel", ],
+    S => ["sul", "sulletje"],
+    T => ["traag", ],
+    U => ["uberhaupt", ],
+    V => ["vrouw", ],
+    W => ["wanbetaler", ],
+    X => ["xantippevogel", ],
+    Y => ["yahoo", ],
+    Z => ["zeikerd", ],
+    
+);                                                                                                                                                
+
+$pass =~ s/[1-9]/0/g; #just zeros as numbers...
+
+my @chars = split // , $pass;
+
+my %seen;
+my $result;
+foreach my $char (@chars) {
+   next unless (defined $char);
+   if ($char eq '0') {
+     $result .= "het cijfer nul, " 
+   } else {
+     $result .= ( $seen{$char}++ ? 'nogmaals ' : '' ). ($char eq uc($char) ? 'de hoofdletter' : 'de letter') . " $char van " . $table{uc($char)}->[ int rand @{$table{uc($char)}}] . ", ";    
+   }
+}
+
+$result =~ s/, $/./;
+
+print "Uw password is $pass. U spelt dit met ";
+print $result, "\n";
+
+                                                                                                                                           
