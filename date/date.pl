ef9ce3e9c9aae92267f845c758554b3123171733
Bas van Sisseren <bas@dnd.utwente.nl>
2007-10-22 16:29:14 +0000

    better support for multiple time-zones
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@149 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- date/date.pl
+++ date/date.pl
@@ -1,3 +1,32 @@
 #!/usr/bin/perl -w
-$ENV{TZ} = $ARGV[0] if $ARGV[0];
-print scalar localtime() . "\n"
+
+my $tz = @ARGV ? $ARGV[0] : '';
+
+my @tz_groups = qw/
+		Africa America Antarctica Arctic
+		Asia Atlantic Australia Brazil
+		Canada Chile Europe Indian
+		Mexico Mideast Pacific Etc
+	/;
+
+if ($tz ne '') {
+	$tz =~ s/ /_/g;
+	if ( $tz !~ /\A(?:\w+\/)*\w+\z/) {
+		print "Invalid timezone syntax.\n";
+		exit 1;
+	} elsif ( -f '/usr/share/zoneinfo/'.$tz ) {
+		$ENV{TZ} = $tz;
+	} else {
+		foreach my $tzg (@tz_groups) {
+			if ( -f '/usr/share/zoneinfo/'.$tzg.'/'.$tz ) {
+				$ENV{TZ} = $tzg . '/' . $tz;
+				print scalar localtime() . "\n";
+				exit 0;
+			}
+		}
+		print "Unknown timezone.\n";
+		exit 1;
+	}
+}
+
+print scalar localtime() . "\n";
