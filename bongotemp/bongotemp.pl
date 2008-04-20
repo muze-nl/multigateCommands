682fb2938d41951060868c9bafb703e3cc65cc88
Casper Joost Eyckelhof <joost@multigator.net>
2008-04-20 17:03:54 +0000

    weather conditions added (by tony)
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@162 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- bongotemp/bongotemp.pl
+++ bongotemp/bongotemp.pl
@@ -2,9 +2,19 @@
 use strict;
 use warnings;
 
+
 my $line = `lynx -dump http://www.bonairewebcams.com/CurrentWeather.php | grep Temperature`;
+#my $line = `lynx -dump http://weather.noaa.gov/weather/current/TNCB.html | grep "Temperature "`;
 
 $line =~ m/\((\d+.\d+).*?\)/;
 my $temp = $1;
 
+#print substr($line,15)  ;;
 print "Huidige weersituatie in tonytee's toekomstige achtertuin: " . $temp . " graden Celsius\n";;
+
+sleep 1 ;
+
+my $line2 = `lynx -dump http://weather.noaa.gov/weather/current/TNCB.html | grep "Sky conditions "`;
+print  "En oja, het is er " . substr($line2,18) ;;
+
+
