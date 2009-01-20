f6b4ad59f9b79600408869213fb98dbc979737b7
Bas van Sisseren <bas@dnd.utwente.nl>
2009-01-20 15:49:06 +0000

    oops.. :)
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@190 07fb6d0f-9701-0410-8146-bd1aaf089aca

---
+++ radio2/radio2.pl
@@ -0,0 +1,29 @@
+#! /usr/bin/perl -w
+
+use strict;
+use LWP::UserAgent;
+use URI;
+
+use XML::Simple;
+
+my $url = "http://www.radio2.nl/data/dalet/onair.xml";
+
+my $ua = new LWP::UserAgent;
+
+#Set agent name, we are not a script! :)
+my $agent = "Mozilla/4.0 (compatible; MSIE 4.01; Windows 98)";
+$ua->agent($agent);
+
+my $request = new HTTP::Request( 'GET', $url );
+my $content = $ua->request($request)->content;
+
+my $data = XMLin($content);
+
+my ($song, $artist, $album) = ($data->{'Current'}->{'titleName'}, $data->{'Current'}->{'artistName'}, $data->{'Current'}->{'albumName'});
+if ($song ne "" and $artist ne "" and $album ne "") {
+	print "$artist - $song  [Album: $album]\n";
+}
+else {
+	print "Sorry, no information available...\n";
+}
+
