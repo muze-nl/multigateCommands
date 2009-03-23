1420a969ab52419c3a4a2429df74477f71951f84
Casper Joost Eyckelhof <joost@multigator.net>
2009-03-23 10:00:44 +0000

    website changed
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@194 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- netnummer/netnummer.pl
+++ netnummer/netnummer.pl
@@ -7,7 +7,7 @@ use LWP::UserAgent;
 
 my $nummer = $ARGV[0];
 
-my $url = "http://www.detelefoongids.nl/netnummers.html";
+my $url = "http://www.detelefoongids.nl/static/netnummers.html";
 
 my $result = "fout: nummer niet gevonden, of !netnummer stuk...";
 if (defined $nummer and $nummer =~ m/0\d+/) {
@@ -17,7 +17,7 @@ if (defined $nummer and $nummer =~ m/0\d+/) {
 	my $request = new HTTP::Request( 'GET', $url );
 	my $content = $ua->request($request)->content;
 	if (defined $content) {
-		($result) = $content =~ m!<td>$nummer</td>\s*<td>\s*([^<]*?)\s*</td>!im;
+		($result) = $content =~ m!<td class="name">$nummer</td>\s*<td>\s*([^<]*?)\s*</td>!im;
 		$result = "niet gevonden" unless defined $result;
 	}
 } else {
