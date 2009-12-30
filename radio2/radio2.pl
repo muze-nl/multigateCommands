00cb41b10ccc8744612f2d7074fe1d6e7e23a66f
Casper Joost Eyckelhof <joost@multigator.net>
2009-12-30 15:41:59 +0000

    allow commandline 'next' to display next song
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@211 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- radio2/radio2.pl
+++ radio2/radio2.pl
@@ -6,20 +6,26 @@ use URI;
 
 use XML::Simple;
 
-my $url = "http://www.radio2.nl/data/dalet/onair.xml";
+my $commandline = $ARGV[0];
+$commandline = (defined $commandline) ? $commandline : '';
 
-my $ua = new LWP::UserAgent;
+my $index = 'Current'; # XML contains 'Current' and 'Next'
+if ($commandline =~ /next/ ) {  #somewhere  
+  $index = 'Next';
+}
 
 #Set agent name, we are not a script! :)
 my $agent = "Mozilla/4.0 (compatible; MSIE 4.01; Windows 98)";
+my $ua = new LWP::UserAgent;
 $ua->agent($agent);
 
+my $url = "http://www.radio2.nl/data/dalet/onair.xml";
 my $request = new HTTP::Request( 'GET', $url );
 my $content = $ua->request($request)->content;
 
 my $data = XMLin($content);
 
-my ($song, $artist, $album) = ($data->{'Current'}->{'titleName'}, $data->{'Current'}->{'artistName'}, $data->{'Current'}->{'albumName'});
+my ($song, $artist, $album) = ($data->{$index}->{'titleName'}, $data->{$index}->{'artistName'}, $data->{$index}->{'albumName'});
 
 #check for references in xml-structure, we want strings!
 $artist = 'none' if (ref $artist ne '');
