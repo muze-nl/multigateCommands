098b1a78f7736803e21f0b56d8b08a912a605787
Casper Joost Eyckelhof <joost@multigator.net>
2010-12-13 09:40:15 +0000

    updates by ado to fix regexp
    git-svn-id: https://svn.muze.nl/svn/multigate_commands/trunk@232 07fb6d0f-9701-0410-8146-bd1aaf089aca

--- weather/weather.pl
+++ weather/weather.pl
@@ -6,12 +6,17 @@ use strict;
 use LWP::UserAgent;
 use URI::Escape;
 use HTML::Entities;
+use XML::Simple;
+
 
 #
 # SEARCH CITY
 #
-my $where = uri_escape(uc($ARGV[0]));
-my $url = "http://weather.cnn.com/weather/citySearch?search_term=$where&filter=true&csiID=csi2";
+my $where = "Twenthe,Netherlands";
+if (defined $ARGV[0]) {
+	$where = uri_escape($ARGV[0]);
+}
+my $url = "http://weather.cnn.com/weather/citySearch?search_term=$where&filter=true";
 
 # Create a new useragent instance
 my $ua = new LWP::UserAgent;
@@ -22,53 +27,52 @@ $ua->agent($agent);
 
 # Fetch the requested page
 my $httpresult = $ua->get($url);
-if (!$httpresult->is_success) {
-	print "Error opening cities search page\n";
+unless ($httpresult->is_success) {
+	print "Error opening cities search page: ";
+	print $httpresult->status_line, "\n";
 	exit 1;
 }
 my $content = $httpresult->content;
 
-# Pfah, we need no steenking XML parser! Nothing a bit of regexp abracadabra can't do
-my @locations;
-while ($content =~ m|<LOCATION\s*?code\s*?=\s*?\"(.*?)\"\s*?city\s*?=\s*?\"(.*?)\"\s*?stateorcountry\s*?=\s*?\"(.*?)\"\s*?zip\s*?=\s*?\"(.*?)\"\s*?/>|sg) {
-	my $locinfo = {};
-	$locinfo->{'code'} = $1;
-	$locinfo->{'city'} = decode_entities($2);
-	$locinfo->{'soc'} = decode_entities($3);
-	$locinfo->{'zip'} = $4;
-	push(@locations, $locinfo)
+# And parse the returned XML 
+my $xmlref = XMLin($content, ForceArray => 1);
+
+unless (%$xmlref) {
+	# An empty list means that nothing was found
+	print "Could not find a city matching $where.\n";
+	exit 0;
 }
 
-if (scalar(@locations) == 0) {
-	# Hmm, nothing found
-	print "Could not find a city matching $ARGV[0].\n";
-	exit 0;
-} elsif (scalar(@locations) > 1) {
+if (scalar @{$xmlref->{'LOCATION'}} > 1) {
 	# Oh noes! Too many cities found
 	print "Multiple possibilities: ";
-	for my $i (0 .. $#locations) {
-		if ($i) {
+	my $sep = 0;
+	foreach my $location (@{$xmlref->{'LOCATION'}}) {
+		if ($sep) {
 			print "; ";
 		}
-		print $locations[$i]->{'city'}, ",", $locations[$i]->{'soc'};
+		print "$location->{'city'},$location->{'stateorcountry'}";
+		$sep = 1;
 	}
 	print "\n";
 	exit 0;
 }
 
-# This must be the place
-my $code = $locations[0]->{'code'};
-my $city = $locations[0]->{'city'};
-my $soc = $locations[0]->{'soc'};
-my $zip = $locations[0]->{'zip'};
+# Well, only one left. This must be the place
+my $code = $xmlref->{'LOCATION'}[0]->{'code'};
+my $city = $xmlref->{'LOCATION'}[0]->{'city'};
+my $soc = $xmlref->{'LOCATION'}[0]->{'stateorcountry'};
+my $zip = $xmlref->{'LOCATION'}[0]->{'zip'};
+
 
 #
 # GET WEATHER INFO
 #
-$url = "http://weather.cnn.com/weather/forecast.jsp?celcius=true&locCode=$code&zipCode=$zip";
+$url = "http://weather.cnn.com/weather/intl/forecast.jsp?celcius=true&locCode=$code&zipCode=$zip";
 $httpresult = $ua->get($url);
-if (!$httpresult->is_success) {
-	print "Error opening weather information page for $city, $soc\n";
+unless ($httpresult->is_success) {
+	print "Error opening weather information page for $city, $soc: ";
+	print $httpresult->status_line, "\n";
 	exit 1;
 }
 $content = $httpresult->content;
@@ -77,85 +81,76 @@ $content = $httpresult->content;
 # CURRENT
 # 
 # This is (roughly) the current condition part
-$content =~ m|.*cnnWeatherForecastCurrentContent(.*?)cnnWeatherForecastCurrentDetails.*|s;
+$content =~ m|cnnWeatherForecastCurrentContent(.*?)cnnWeatherForecastCurrentDetails|s;
 my $currentpart = $1;
-# Extract the temperature
-$currentpart =~ m|.*cnnWeatherTempCurrent\">(-*\d+)&deg;.*|s;
+# Extract the temperature and condition
+$currentpart =~ m|cnnWeatherTempCurrent\">\s*(-*\d+)&deg;.*cnnWeatherConditionCurrent\">\s*(.*)\s*</span>|s;
 my $currenttemp = $1;
-# And the condition
-$currentpart =~ m|.*cnnWeatherConditionCurrent\">(.*)</span>.*|s;
-my $currentcond = decode_entities($1);
-$currentcond =~ s/^\s*//;
-$currentcond =~ s/\s*$//;
+my $currentcond = decode_entities($2);
+
+
+my @forecastlist;
 
 #
 # TOMORROW
 #
-my @forecastlist;
-my $fcinfo = {};
+my $fcitem = {};
 # This is (roughly) the tomorrow forecast part
-$content =~ m|.*cnnWeatherHeader\">Tomorrow(.*?)cnnWireBoxFooter.*|s;
+$content =~ m|cnnWeatherHeader\">Tomorrow(.*?)cnnWireBoxFooter|s;
 my $tomorrowpart = $1;
 $tomorrowpart =~ m|cnnWeatherTimeStamp\">(.*?)</span>|s;
-$fcinfo->{'day'} = substr($1, 0, 3);
-$fcinfo->{'temps'} = ();
-# Extract the temperatures
-while ($tomorrowpart =~ m|cnnWeatherTemp\">[HiLo ]+(-*\d+)&deg;</span>|gs) {
-	push(@{$fcinfo->{'temps'}}, $1);
-}
-# And the condition
-$tomorrowpart =~ m|.*cnnWeatherCondition\">(.*)</span>.*|s;
-my $tomorrowcond = decode_entities($1);
-$tomorrowcond =~ s/^\s*//;
-$tomorrowcond =~ s/\s*$//;
-$fcinfo->{'cond'} = $tomorrowcond;
-push(@forecastlist, $fcinfo);
+# Use only first three letters of day to match extended forecast
+$fcitem->{'day'} = substr($1, 0, 3);
+# Extract the temperatures and condition
+$tomorrowpart =~ m|cnnWeatherTemp\">\s*Hi\s*(-*\d+)&deg;.*cnnWeatherTemp\">\s*Lo\s*(-*\d+)&deg;.*cnnWeatherCondition\">\s*(.*)\s*</span>|s;
+$fcitem->{'temphi'} = $1;
+$fcitem->{'templo'} = $2;
+$fcitem->{'cond'} = decode_entities($3);
+push(@forecastlist, $fcitem);
 
 #
 # EXTENDED
 #
 # This is (roughly) the extended forecast part
-$content =~ m|.*<table class=\"cnnWeatherExtForecast\"(.*?)table.cnnWeatherExtForecast.*|s;
+$content =~ m|<table class=\"cnnWeatherExtForecast\"(.*?)tableCornerBL|s;
 my $extendedpart = $1;
-# Loop over the table header to get the days
+# Loop over the table header to get the names of the days
 my @extdayslist;
 while ($extendedpart =~ m|<td class=\".*?\">\s*?(\w+?)\s*?<br />|gs) {
 	push(@extdayslist, $1);
 }
-# Next loop over the table cells to get temperature and condition
+# Next loop over the table cells to get the temperatures and condition belonging to each day
 my @extcondlist;
-while ($extendedpart =~ m|cnnWeatherExtForecastDetails\">\s*?(-*\d+)&deg;.*?</span>\s*?(-*\d+)&deg;.*?cnnWeatherExtForecastDayCond\">(.*?)</span>|gs) {
+while ($extendedpart =~ m|cnnWeatherExtForecastDetails\">\s*?(-*\d+)&deg;.*?</span>\s*?(-*\d+)&deg;.*?cnnWeatherExtForecastDayCond\">\s*(.*?)\s*</span>|gs) {
 	my $extcond = {};
-	$extcond->{'temps'} = ();
-	push(@{$extcond->{'temps'}}, $1, $2);
-	my $cond = decode_entities($3);
-	$cond =~ s/^\s*//;
-	$cond =~ s/\s*$//;
-	$extcond->{'cond'} = $cond;
+	$extcond->{'temphi'} = $1;
+	$extcond->{'templo'} = $2;
+	$extcond->{'cond'} = decode_entities($3);
 	push(@extcondlist, $extcond);
 }
 
 # Did we read as many cells as there are days?
 if (scalar(@extdayslist) != scalar(@extcondlist)) {
-	print "Bleh. Forecast data mismatch\n";
-	print "It is $currenttemp C ($currentcond) in $city, $soc\n";
+	print "Bleh. Forecast data mismatch!\n";
+	print "It is $currenttemp C ( $currentcond ) in $city, $soc\n";
 	exit 1;
 }
 
 # OK. Join them and put them in the forecastlist
 for my $i (0 .. $#extdayslist) {
-	$fcinfo = {};
-	$fcinfo->{'day'} = $extdayslist[$i];
-	$fcinfo->{'temps'} = $extcondlist[$i]->{'temps'};
-	$fcinfo->{'cond'} = $extcondlist[$i]->{'cond'};
-	push(@forecastlist, $fcinfo);
+	$fcitem = {};
+	$fcitem->{'day'} = $extdayslist[$i];
+	$fcitem->{'temphi'} = $extcondlist[$i]->{'temphi'};
+	$fcitem->{'templo'} = $extcondlist[$i]->{'templo'};
+	$fcitem->{'cond'} = $extcondlist[$i]->{'cond'};
+	push(@forecastlist, $fcitem);
 }
 
 #
 # OUTPUT
 #
 # Finally tell the world
-print "It is $currenttemp C ($currentcond) in $city, $soc\n";
+print "It is $currenttemp C ( $currentcond ) in $city, $soc\n";
 
 print "Forecast: ";
 my $limit = $#forecastlist;
@@ -167,9 +162,9 @@ for my $i (0 .. $limit) {
 	if ($i) {
 		print "; ";
 	}
-	print $forecastlist[$i]->{'day'}, " ";
-	print $forecastlist[$i]->{'temps'}[0], " | ";
-	print $forecastlist[$i]->{'temps'}[1], " C ";
+	print "$forecastlist[$i]->{'day'} ";
+	print "$forecastlist[$i]->{'temphi'} | ";
+	print "$forecastlist[$i]->{'templo'} ";
 	print $forecastlist[$i]->{'cond'};
 }
 print "\n";
