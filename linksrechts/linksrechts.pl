c069968c4a18ab0d84e72068f0a0d4058267cfcd
Robbert Müller <muller@muze.nl>
2016-04-06 09:17:11 +0200

    some utf8 fun with leftright

--- linksrechts/linksrechts.pl
+++ linksrechts/linksrechts.pl
@@ -1,3 +1,16 @@
 #!/usr/bin/perl -w
-my @janee = qw (links rechts);
-print $janee[ int( rand(2) ) ] . "\n";
+
+use strict;
+use warnings;
+binmode STDOUT, ":utf8";
+
+my @janee = (
+	"links",
+	"\x{2190}",
+	"\x{21FD}",
+	"\x{21B0}",
+	"rechts",
+	"\x{2192}",
+	"\x{21B1}",
+);
+print $janee[ rand(@janee) ] . "\n";
