3ef2b9f54e9143c484890c634c41798685bb0dfb
Koen Zandberg <koen@bergzand.net>
2018-10-04 14:00:57 +0200

    Expand wauwel
    Add words based on https://www.utwente.nl/nieuws/!/2018/10/298378/the-gallery-fase-2-innovatiekatalysator-voor-ondernemende-community

--- wauwel/wauwel.pl
+++ wauwel/wauwel.pl
@@ -5,16 +5,20 @@ my @first = (
     "schematische ",  "formatieve ",   "optimale ",        "gesynchroniseerde ",
     "functionele ",   "optionele ",    "geobjectiveerde ", "uitgekristalliseerde ",
     "normatieve ",    "progressieve ", "decentrale ",      "centrale ",
-    "strategische ",  "pseudo-",       "duurzame ",        "ingebouwde "
+    "strategische ",  "pseudo-",       "duurzame ",        "ingebouwde ",
+    "innoverende", ", "community-driven", "exponentiële",  "circulaire",
+    "revitaliserende"
 );
 my @second = (
     "beleids",   "rationaliserings", "systeem",       "management",  "productiviteits", "normaliserings",
-    "structuur", "utiliteits",       "ontwikkelings", "organisatie", "smart-"
+    "structuur", "utiliteits",       "ontwikkelings", "organisatie", "smart-", "herstructurerings",
+    "innovatie", "concept"
 );
 my @third = (
     "standaardisatie", "synthese",  "inschaling",   "mobiliteit",    "analyse",    "programmering",
     "fasering",        "projectie", "stabilisatie", "flexibiliteit", "rapportage", "participatie",
-    "bevordering",     "blockchain"
+    "bevordering",     "blockchain", "maatschappij", "programma's",  "ontwikkeling", "omgeving"
+    
 );
 
 my $result = $first[ rand(@first) ] . $second[ rand(@second) ] . $third[ rand(@third) ];
